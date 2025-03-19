4508835c0046e2f57c286be858192d13d328ab63
Deepak <vakkaladeepak145@gmail.com>
2025-03-19 13:41:54 +0530

    Added Module

---
+++ Module/VPC/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1" # Change this as needed
+}
+
+module "vpc" {
+  source            = "./modules/"
+  vpc_cidr         = "10.0.0.0/16"
+  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones = ["us-west-2", "us-west-2"]
+}
